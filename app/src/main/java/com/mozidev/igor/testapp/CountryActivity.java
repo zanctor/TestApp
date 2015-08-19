164cc927d629a9f19f5801db65215c6fc11cb74c
zanctor <1tshark1@mail.ru>
2015-08-19 17:26:45 +0300

    returning realised

--- app/src/main/java/com/mozidev/igor/testapp/CountryActivity.java
+++ app/src/main/java/com/mozidev/igor/testapp/CountryActivity.java
@@ -1,17 +1,26 @@
 package com.mozidev.igor.testapp;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 
 /**
  * Created by igor on 19.08.15.
  */
-public class CountryActivity extends Activity {
+public class CountryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_country);
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/mozidev/igor/testapp/MainActivity.java
+++ app/src/main/java/com/mozidev/igor/testapp/MainActivity.java
@@ -1,15 +1,14 @@
 package com.mozidev.igor.testapp;
 
-
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import java.util.*;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
